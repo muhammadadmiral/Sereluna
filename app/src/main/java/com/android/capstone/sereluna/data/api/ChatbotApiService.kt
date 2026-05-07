3b7eab5c3e4d0f8a89906a77ab2f66bf2da1debe
muhammadadmiral <admelar15@gmail.com>
2026-05-08 03:54:14 +0700

    update FULL_SCRIPT_URL in ChatbotApiService

--- app/src/main/java/com/android/capstone/sereluna/data/api/ChatbotApiService.kt
+++ app/src/main/java/com/android/capstone/sereluna/data/api/ChatbotApiService.kt
@@ -46,7 +46,7 @@ interface ChatbotApiService {
     fun sendMessage(@Url url: String, @Body request: ChatRequest): Call<ChatResponse>
 
     companion object {
-        const val FULL_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwoVi39-hqUwSNjNMoBWLhV8bq9od3KgP8wGU2dtyBNL0al-DdCGRanidsNxWE7reQS/exec"
+        const val FULL_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzdTdXAz0jFNtMpYXpUOMd7leNFBLQpcAcvWJKHUflX1UAmZLadegnSZnsL9TuqADnn/exec"
 
         fun create(): ChatbotApiService {
             val logging = HttpLoggingInterceptor()
